31f1ec73bc0b221b922c2c75fb32772c7da2f9f3
Kevin DeLand <kevin.deland@gmail.com>
2020-12-18 17:24:30 -0500

    Refactor Field creators out of setup()

--- sketch.js
+++ sketch.js
@@ -20,6 +20,93 @@ InitColors = function() {
 }
 // *****************************
 
+
+// *****************************
+// ******* Field Creator *******
+// *****************************
+
+let VectorFields, StartingPoints;
+
+InitVectorFields = function() {
+    
+  VectorFields = {
+    'noise1': function(col, row) {
+      noiseDetail(10, 0.4)
+      const scaled_x = col * 0.005
+      const scaled_y = row * 0.005
+      const noise_val = noise(scaled_x, scaled_y)
+      let angle = map(noise_val, 0.0, 1.0, 0.0, TWO_PI)
+      return angle
+    },
+
+    'noise_concrete': function(col, row) {
+      noiseDetail(10, 0.8)
+      const scaled_x = col * 0.005
+      const scaled_y = row * 0.005
+      const noise_val = noise(scaled_x, scaled_y)
+      const STEPS = 4
+      let concrete = int(map(noise_val, 0.0, 1.0, 0.0, STEPS))
+
+      return concrete / STEPS * TWO_PI
+    },
+
+    'total_random': function(col, row) {
+      return random(TWO_PI)
+    },
+
+    'test': function(col, row) {
+      return map(col, 0, num_columns, 0, TWO_PI)
+    }
+  }
+
+
+  StartingPoints = {
+    'grid': function() {
+      let xs = count(-10, 30).map(function(n) { return n*50 })
+      let ys = count(-10, 30).map(function(n) { return n*50 })
+    
+      let cs = count(-10, 30).map(function(n) { 
+        // console.log(n)
+        let index = (n+10)%MainPattern.length
+        // console.log(index)
+        return MainPattern[index]
+      })
+
+      return {xs, ys, cs}
+    },
+
+    'grid2': function() {
+      const res = 25
+      const x1 = -20;
+      const x2 = 60;
+      let xs = count(x1, x2).map(function(n) { return n*res })
+      let ys = count(x1, x2).map(function(n) { return n*res })
+    
+      let cs = count(x1, x2).map(function(n) { 
+        // console.log(n)
+        let index = (n-x1)%MainPattern.length
+        // console.log(index)
+        return MainPattern[index]
+      })
+
+      return {xs, ys, cs}
+    },
+
+    'random': function() {
+      let xs = [], ys = [], cs = [];
+      for (let i=0; i < 30; i++) {
+        xs.push(int(random(-500, 1500)))
+        ys.push(int(random(-500, 1500)))
+        cs.push('bl2')
+      }
+      return {xs, ys, cs}
+    }
+
+  }
+}
+
+
+
 // *****************************
 // ******* Pattern Bank ********
 // *****************************
@@ -43,6 +130,15 @@ InitPatternBank = function() {
   }
 }
 
+// Boundaries
+let left_x, right_x, top_y, bottom_y;
+
+let resolution;
+let num_columns, num_rows;
+let grid;
+
+let MainPattern;
+
 
 function setup() {
   CW = 1000, CH = 1000
@@ -51,7 +147,10 @@ function setup() {
   colorMode(HSB, 16)
   InitColors()
   InitPatternBank()
+
+  InitVectorFields()
   
+  // Extend Flow past the edges
   left_x = int(CW * -0.5)
   right_x = int(CW * 1.5)
   top_y = int(CH * -0.5)
@@ -62,40 +161,10 @@ function setup() {
   num_columns = (right_x - left_x) / resolution
   num_rows = (bottom_y - top_y) / resolution
   
-  let gr = []
+  grid = []
 
   let default_angle = QUARTER_PI
   
-  const VectorFunctions = {
-    
-    'noise1': function(col, row) {
-      noiseDetail(10, 0.4)
-      scaled_x = col * 0.005
-      scaled_y = row * 0.005
-      noise_val = noise(scaled_x, scaled_y)
-      let angle = map(noise_val, 0.0, 1.0, 0.0, TWO_PI)
-      return angle
-    },
-
-    'noise_concrete': function(col, row) {
-      noiseDetail(10, 0.8)
-      scaled_x = col * 0.005
-      scaled_y = row * 0.005
-      noise_val = noise(scaled_x, scaled_y)
-      const STEPS = 4
-      let concrete = int(map(noise_val, 0.0, 1.0, 0.0, STEPS))
-
-      return concrete / STEPS * TWO_PI
-    },
-
-    'total_random': function(col, row) {
-      return random(TWO_PI)
-    },
-
-    'test': function(col, row) {
-      return map(col, 0, num_columns, 0, TWO_PI)
-    }
-  }
   // INITIALIZE GRID
   count(0, num_columns).forEach(function(col) {
     let xc = []
@@ -103,82 +172,48 @@ function setup() {
 
       /* let angle = (row / float(num_rows)) * PI */
 
-      let angle = VectorFunctions['noise1'](col,row)
+      let angle = VectorFields['noise_concrete'](col,row)
       xc.push(angle)
     })
-    gr.push(xc)
+    grid.push(xc)
   })
 
   // DRAW ANGLES
   count(0, num_columns).forEach(function(col) {
     count(0, num_rows).forEach(function(row) {
-      let angle = gr[col][row]
+      let angle = grid[col][row]
 
       // drawFlowLine(left_x + col*resolution, top_y + row*resolution, angle)
     })
   })
 
-  const Pattern = PatternBank['stripes']
+  MainPattern = PatternBank['stripes']
 
-  const StartPointFunctions = {
-    'grid': function() {
-      let xs = count(-10, 30).map(function(n) { return n*50 })
-      let ys = count(-10, 30).map(function(n) { return n*50 })
-    
-      let cs = count(-10, 30).map(function(n) { 
-        // console.log(n)
-        let index = (n+10)%Pattern.length
-        // console.log(index)
-        return Pattern[index]
-      })
+  const paths = StartingPoints['grid2']()
 
-      return {xs, ys, cs}
-    },
-
-    'grid2': function() {
-      const res = 25
-      const x1 = -20;
-      const x2 = 60;
-      let xs = count(x1, x2).map(function(n) { return n*res })
-      let ys = count(x1, x2).map(function(n) { return n*res })
-    
-      let cs = count(x1, x2).map(function(n) { 
-        // console.log(n)
-        let index = (n-x1)%Pattern.length
-        // console.log(index)
-        return Pattern[index]
-      })
-
-      return {xs, ys, cs}
-    }
-
-  }
-
-  const paths = StartPointFunctions['grid2']()
   
   const xs = paths.xs
   const ys = paths.ys
   const cs = paths.cs
+  console.log(cs)
 
   count(0, xs.length).forEach(function(i) {
     let x_start = xs[i]
-    console.log('x_start')
-    console.log(x_start)
+    
     ys.forEach(function(y_start) {
       let num_steps = 10;
       let step_length = 20;
 
       let x_draw = x_start
       let y_draw = y_start
-      // console.log(y_start)
 
       beginShape()
       noFill()
       count(0, num_steps).forEach(function(n) {
         strokeWeight(4)
+
         stroke(ColorPalette[cs[i]])
-        // fill('blue')
-        // circle(x_draw, y_draw, 5)
+
         vertex(x_draw, y_draw)
     
         x_offset = x_draw - left_x
@@ -190,7 +225,7 @@ function setup() {
         // check bounds
         if (column_index >= num_columns || column_index < 0 || 
           row_index < 0 || row_index >= num_rows) return;
-        grid_angle = gr[column_index][row_index]
+        grid_angle = grid[column_index][row_index]
     
         x_step = step_length * cos(grid_angle)
         y_step = step_length * sin(grid_angle)
@@ -201,10 +236,6 @@ function setup() {
       endShape()
     })
   })
-
-
-  
- console.log(gr) 
 }
 
 function draw() {
